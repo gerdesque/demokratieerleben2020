2e6340cae29a1514bc02098ccbcd4f82b08a6678
Gerd Müller <g.mueller@ewerk.com>
2020-04-23 10:23:42 +0200

    Add home lib

--- gerdesque/apps/demol/src/app/app.tsx
+++ gerdesque/apps/demol/src/app/app.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Home } from '@gerdesque/home';
 import { Admin, Resource } from 'react-admin';
 import { ChapterList, ChapterEdit, ChapterCreate } from './admin/chapter';
 import jsonServerProvider from 'ra-data-json-server';
@@ -7,15 +9,22 @@ import './app.scss';
 const dataProvider = jsonServerProvider('http://localhost:3000');
 export const App = () => {
   return (
-    <Admin dataProvider={dataProvider}>
-      <Resource
-        name="chapter"
-        options={{ label: 'Kapitel' }}
-        list={ChapterList}
-        edit={ChapterEdit}
-        create={ChapterCreate}
-      />
-    </Admin>
+    <BrowserRouter>
+      <div className="app">
+        <nav className="app-nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="app-content">
+          <Route path="/" exact component={Home} />
+          <Route path="/admin" exact component={Admin} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ gerdesque/libs/home/src/lib/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './home.scss';
+
+/* eslint-disable-next-line */
+export interface HomeProps {}
+
+export const Home = (props: HomeProps) => {
+  return (
+    <div>
+      <h1>Demokratie erleben</h1>
+    </div>
+  );
+};
+
+export default Home;
