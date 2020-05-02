d599cc9fe47b6c4e41b5893c9e1e9cc4e2134a49
Gerd Müller <gerdesk@posteo.org>
2020-05-02 17:26:46 +0200

    Remove workspace folder

---
+++ apps/demol/src/app/app.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from '@gerdesque/home';
+import { Adminui } from '@gerdesque/adminui';
+import { Footer, Nav, Chapter, Title } from '@gerdesque/ui';
+
+import './app.scss';
+
+export const App = () => {
+
+  const [chapters, setChapters] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/chapter')
+      .then(_ => _.json())
+      .then(setChapters);
+  }, []);
+
+  const renderChapter = (routerProps) => {
+    const chapterId = parseInt(routerProps.match.params.id)
+    const chapter = chapters.find(chapterObj => chapterObj.id === chapterId)
+    return (chapter && 
+      <Chapter class={chapter.name}>
+        <Title text={chapter.name} />
+        <div className='box'>
+          <p>{chapter.path}</p>
+        </div>
+      </Chapter>)
+  }
+
+  return (
+    <BrowserRouter basename="/demokratieerleben2020">
+      <div className="app">
+        <Nav chapters = {chapters}/>
+        <div className="app-content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/adminui" exact component={Adminui} />
+            <Route path="/chapter/:id" render={renderChapter} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/footer/footer';
 export * from './lib/nav/nav';
 export * from './lib/title/title';
 export * from './lib/chapter/chapter';
