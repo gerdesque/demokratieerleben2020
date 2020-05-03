f8ff6b10f081c784a16f65a42f2c81602e5b710d
Gerd Müller <gerdesk@posteo.org>
2020-05-03 18:07:16 +0200

    Add 'content' field for AdminUI

--- libs/adminui/src/lib/adminui.tsx
+++ libs/adminui/src/lib/adminui.tsx
@@ -3,11 +3,13 @@ import { createBrowserHistory as createHistory } from 'history';
 import { Admin, Resource } from 'react-admin';
 import { ChapterList, ChapterEdit, ChapterCreate } from './chapter';
 import jsonServerProvider from 'ra-data-json-server';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { API_URL } from '@gerdesque/data';
 
 import './adminui.scss';
 
 const history = createHistory();
-const dataProvider = jsonServerProvider('http://localhost:3000');
+const dataProvider = jsonServerProvider(API_URL);
 
 /* eslint-disable-next-line */
 export interface AdminuiProps {}
--- libs/adminui/src/lib/chapter.tsx
+++ libs/adminui/src/lib/chapter.tsx
@@ -3,21 +3,27 @@ import {
   List,
   Datagrid,
   TextField,
+  ArrayField,
+  SingleFieldList,
+  ChipField,
   Edit,
   SimpleForm,
   TextInput,
   ReferenceInput,
   SelectInput,
   Create,
-  EditButton
+  EditButton,
+  ArrayInput,
+  SimpleFormIterator,
+  RadioButtonGroupInput
 } from 'react-admin';
 
 export const ChapterList = props => (
   <List {...props}>
     <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="path" />
+      <TextField label="Name" source="name" />
+      <TextField label="Pfad" source="link" />
+      <ArrayField label="Inhalt" source="content"><SingleFieldList><ChipField source="type" /></SingleFieldList></ArrayField>
       <EditButton />
     </Datagrid>
   </List>
@@ -28,7 +34,19 @@ export const ChapterEdit = props => (
     <SimpleForm>
       <TextInput disabled source="id" />
       <TextInput source="name" />
-      <TextInput source="path" />
+      <TextInput source="link" />
+      <ArrayInput source="content">
+        <SimpleFormIterator>
+          <RadioButtonGroupInput label="Typ" source="type" choices={[
+            { id: 'text', name: 'Text' },
+            { id: 'video', name: 'Video' },
+            { id: 'image', name: 'Bild' },
+            { id: 'game', name: 'Spiel' },
+            { id: 'misc', name: 'Sonstiges' },
+          ]} />
+          <TextInput multiline label="Wert" fullWidth="true" source="value" />
+        </SimpleFormIterator>
+      </ArrayInput>
     </SimpleForm>
   </Edit>
 );
@@ -40,7 +58,7 @@ export const ChapterCreate = props => (
         <SelectInput optionText="name" />
       </ReferenceInput>
       <TextInput source="name" />
-      <TextInput source="path" />
+      <TextInput source="link" />
     </SimpleForm>
   </Create>
 );
