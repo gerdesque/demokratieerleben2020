13cde6a0d298793cbb6d398f1b4c002cac14298f
Gerd Müller <gerdesk@posteo.org>
2020-05-10 20:24:54 +0200

    Add dashboard screen

---
+++ apps/admin/src/app/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Title } from 'react-admin';
+export default () => (
+    <Card>
+        <Title title="Willkommen zur Demokratie erLeben Administratoroberfläche" />
+        <CardContent>Hier können Kapitel, Bilder, Spiele und Videos bearbeitet werden.</CardContent>
+    </Card>
+);
