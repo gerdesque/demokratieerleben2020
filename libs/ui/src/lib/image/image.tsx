240ab16adb719c7ecedc56472bab4722bc854ef6
Gerd Müller <gerdesk@posteo.org>
2020-06-26 17:22:50 +0200

    feat(image): add avatar images

--- libs/ui/src/lib/image/image.tsx
+++ libs/ui/src/lib/image/image.tsx
@@ -1,9 +1,8 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import { IMAGE_SUFFIX, IMAGE_SUFFIX_ALTERNATE } from '@gerdesque/data';
 import {AppContext } from '../chapter/context';
 import './image.scss';
 
-/* eslint-disable-next-line */
 export interface ImageProps {
   value: string
   layer?: string
@@ -12,18 +11,17 @@ export interface ImageProps {
 
 export const Image = (props: ImageProps) => {
   const [character, setCharacter] = useContext(AppContext);
-  const [hover, setHover] = useState(false);
-  //const [source, setSource] = useState(props.value);
 
   const chooseCharacter = () => {
     props.title && setCharacter(props.title);
   }
 
   return (
-    <picture className={`chapter_picture ${props.layer} ${props.title && hover ? 'highlighted' : ''}`} onMouseOut={() => setHover(false)} onMouseOver={() => setHover(true)} onClick={() => chooseCharacter()}>
+    <picture className={`chapter_picture ${props.layer}`} onClick={() => chooseCharacter()}>
       <source srcSet={"./assets/"+props.value+IMAGE_SUFFIX} type='image/webp' />
       <source srcSet={"./assets/fallback/"+props.value+IMAGE_SUFFIX_ALTERNATE} type='image/png' />
-      <img draggable="false" src={"./assets/fallback/"+props.value+IMAGE_SUFFIX_ALTERNATE} alt={props.value} />
+      <img draggable="false" src={"./assets/fallback/"+props.value+IMAGE_SUFFIX_ALTERNATE} alt={props.title || props.value} />
+      {props.title && <div className="overlay">{props.title}</div>}
     </picture>
   )
 
