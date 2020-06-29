c7dcbcb640701a23e9c742bc3e8ea29bdbd0192c
Gerd Müller <gerdesk@posteo.org>
2020-06-30 01:07:22 +0200

    feat(app): add background images

--- libs/home/src/lib/home.tsx
+++ libs/home/src/lib/home.tsx
@@ -9,58 +9,55 @@ const home:ChapterProps = {
   {
     "grouped": false,
     "row": false,
+    "background": "2_31_b",
     "content": [
       {
         "type": "text",
-        "value": "Natürlich besteht die Kinderrepublik nicht nur aus Karl, Anna und Gerda. Damit ihr mit den beiden dieses Abenteuer erleben könnt, hat Florian eine Website entworfen, die Gerd umgesetzt hat. Anne hat dafür unter dem Einsatz von Felix‘ Sprecherstimme Trickfilme produziert.",
-        "layer": "fore"
+        "value": "Diese Website erzählt die Geschichte von Anna, Karl und Gerda. Alle drei entstammen der Arbeiterklasse und träumen Ende der 1920er Jahre von einer besseren Zukunft. Begleitet sie auf ihrem Weg in ein Zeltlager der sozialdemokratischen Organisation der 'Reichsarbeitsgemeinschaft der Kinderfreunde'. In dieser so genannten 'Kinderrepublik' erleben sie viele spannende Dinge, und lernen Demokratie durch eigenes Handeln kennen.",
+        "option": "reveal"
       }
     ]
   },
   {
     "grouped": true,
+    "row": false,
+    "content": [{
+      "type": "image",
+      "value": "02_1455_(114)",
+      "option": "second"
+    },
+    {
+      "type": "text",
+      "value": "Die Reise zur Kinderrepublik wäre für Anna, Karl und Gerda ohne Anne Mühlich und Gerd Müller nicht möglich gewesen. Im Rahmen des Kulturhackathons 'Coding da Vinci Westfalen-Ruhrgebiet' 2019 entwickelten die beiden diese Scrollytelling-Website, die auf Fotos und Material des Archivs der Arbeiterjugendbewegung basiert. 2020 haben sie im Rahmen eines Stipendiums des Coding da Vinci die Website für die Zwecke des Archivs ergänzt und ausgebaut."
+    }
+  ]
+},
+  {
+    "grouped": true,
     "row": true,
-    "content": [
-      {
+    "content": [{
         "type": "text",
-        "value": "Anne Mühlich",
-        "option": "avatar second"
+        "value": "Die beiden wurden unterstützt durch das wunderbare Team des Coding da Vinci, die Kolleg*innen des Archivs der Arbeiterjugendbewegung und besonders durch die tatkräftige Mitarbeit der folgenden Personen:"
+      },
+      {
+        "type": "image",
+        "value": "B41ABEA7-64DA-4D8E-8E1B-F55A38DD6E37",
+        "option": "second"
       },
       {
         "type": "text",
-        "value": "Gerd Müller",
-        "option": "avatar second"
-      },
-      {
-        "type": "text",
-        "value": "Julia Kothe",
-        "option": "avatar"
-      },
-      {
-        "type": "text",
-        "value": "Lars Tunçay",
-        "option": "avatar"
-      },
-      {
-        "type": "text",
-        "value": "Nina Heinke",
-        "option": "avatar"
-      },
-      {
-        "type": "text",
-        "value": "Felix Werthschulte",
-        "option": "avatar"
-      }
+        "value": "Julia Kothe, Lars Tunçay, Nina Heinke, Felix Werthschulte, Luise & John",
+        "option": "third"
+      }    
     ]
   },
   {
-    "grouped": false,
-    "row": false,
+    "grouped": true,
+    "row": true,
     "content": [{
-        "type": "text",
-        "value": "Unsere Website richtet sich an all jene, die sich spielerisch mit dem Thema „Kinderrepublik“ und partizipativer Jugendarbeit in der ersten Hälfte des 20. Jahrhunderts beschäftigen wollen und erfahren möchten, wie solche Zeltlager als Form früher und praktischer Demokratiebildung funktioniert haben. In Zeiten wie jetzt, wo mehr und mehr Menschen sich von den demokratischen Institutionen abwenden, lohnt es sich, einen Blick zurückzuwerfen und zu schauen, wie Demokratiebildung insbesondere für bildungsfernere Schichten in der Vergangenheit funktioniert hat und inwiefern wir dafür etwas für unsere heutige Zeit lernen können.",
-        "layer": "fore"
-      }
+      "type": "text",
+      "value": "Die Website richtet sich an all jene, die sich spielerisch mit dem Thema „Kinderrepublik“ und partizipativer Jugendarbeit in der ersten Hälfte des 20. Jahrhunderts beschäftigen wollen und erfahren möchten, wie solche Zeltlager als Form früher und praktischer Demokratiebildung funktioniert haben. In Zeiten wie jetzt, wo mehr und mehr Menschen sich von den demokratischen Institutionen abwenden, lohnt es sich, einen Blick zurückzuwerfen und zu schauen, wie Demokratiebildung insbesondere für bildungsfernere Schichten in der Vergangenheit funktioniert hat und inwiefern wir dafür etwas für unsere heutige Zeit lernen können."
+     }
     ]
   },
   {
@@ -69,7 +66,8 @@ const home:ChapterProps = {
     "content": [{
         "type": "redirect",
         "value": "chapter/daheim",
-        "title": "Demokratie erLeben",
+        "option": "big",
+        "title": "Beginne hier das Abenteuer!",
         "layer": "fore"
       }
     ]
